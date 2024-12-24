778f070a509605f315dc02c7eed1a0bec5161ad0
SasLuca <sas.luca.al@gmail.com>
2024-12-24 14:42:38 +0200

    Improved build system and github workflows
    - Renamed nanoid.zig to root.zig
    - Added examples to build.zig
    - Changed zig source and invocations in cross-build.yml

--- build.zig
+++ build.zig
@@ -5,80 +5,40 @@ pub fn build(b: *std.Build) void
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("nanoid", .{ .root_source_file = b.path("src/nanoid.zig")});
+    const nanoid_mod = b.addModule("nanoid", .{ .root_source_file = b.path("src/root.zig")});
 
-    var main_tests = b.addTest(.{
-        .root_source_file = b.path("src/nanoid.zig"),
+    // Setup default example
+    const default_example_exe = b.addExecutable(.{
+        .name = "default-example",
+        .root_source_file = b.path("examples/default-example.zig"),
+        .optimize = optimize,
+        .target = target,
+    });
+    default_example_exe.root_module.addImport("nanoid", nanoid_mod);
+    const default_example_run = b.addRunArtifact(default_example_exe);
+    const default_example_step = b.step("default-example", "Run a simple example");
+    default_example_step.dependOn(&default_example_run.step);
+
+    // Setup custom alphabet example
+    const custom_alphabet_example_exe = b.addExecutable(.{
+        .name = "custom-alphabet-example",
+        .root_source_file = b.path("examples/custom-alphabet-example.zig"),
+        .optimize = optimize,
+        .target = target,
+    });
+    custom_alphabet_example_exe.root_module.addImport("nanoid", nanoid_mod);
+    const custom_alphabet_example_run = b.addRunArtifact(custom_alphabet_example_exe);
+    const custom_alphabet_example_step = b.step("custom-alphabet-example", "Run a simple example");
+    custom_alphabet_example_step.dependOn(&custom_alphabet_example_run.step);
+
+    // Setup tests
+    const main_tests = b.addTest(.{
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize
     });
+    const main_tests_run = b.addRunArtifact(main_tests);
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    test_step.dependOn(&main_tests_run.step);
 }
-
-// pub fn build(b: *std.Build) void 
-// {
-//     const target = b.standardTargetOptions(.{});
-//     const mode = b.standardReleaseOptions();
-    
-//     const test_step = b.step("test", "Run unit tests directly.");
-//     const run_step = b.step("run", "Run example.");
-    
-//     const default_example = b.option(bool, "default_example", "A simple example of using nanoid.") orelse false;
-//     const custom_alphabet_example = b.option(bool, "custom_alphabet_example", "A simple example of using nanoid.") orelse false;
-//     const tests = b.option(bool, "tests", "The unit tests of the library.") orelse false;
-    
-//     if (default_example)
-//     {
-//         const exe = b.addExecutable("nanoid-zig-default-example", "examples/default-example.zig");
-//         exe.setTarget(target);
-//         exe.setBuildMode(mode);
-//         exe.addPackage(getPackage("nanoid"));
-//         exe.install();
-        
-//         const exe_run = exe.run();
-//         exe_run.step.dependOn(b.getInstallStep());
-//         run_step.dependOn(&exe_run.step);
-//     }
-
-//     if (custom_alphabet_example)
-//     {
-//         const exe = b.addExecutable("nanoid-zig-custom-alphabet-example", "examples/custom-alphabet-example.zig");
-//         exe.setTarget(target);
-//         exe.setBuildMode(mode);
-//         exe.addPackage(getPackage("nanoid"));
-//         exe.install();
-        
-//         const exe_run = exe.run();
-//         exe_run.step.dependOn(b.getInstallStep());
-//         run_step.dependOn(&exe_run.step);
-//     }
-
-//     if (tests)
-//     {
-//         const exe = b.addTestExe("nanoid-zig-test", "src/nanoid.zig");
-//         exe.setTarget(target);
-//         exe.setBuildMode(mode);
-//         exe.install();
-        
-//         const exe_run = exe.run();
-//         exe_run.step.dependOn(b.getInstallStep());
-//         run_step.dependOn(&exe_run.step);
-//     }
-
-//     // Test runner
-//     const test_runner = b.addTest("src/nanoid.zig");
-//     test_runner.setBuildMode(mode);
-//     test_runner.setTarget(target);
-//     test_step.dependOn(&test_runner.step);
-// }
-
-// pub fn getPackage(name: []const u8) std.build.Pkg
-// {
-//     return std.build.Pkg{
-//         .name = name,
-//         .source = .{ .path = comptime std.fs.path.dirname(@src().file).? ++ "/src/nanoid.zig" },
-//         .dependencies = null, // null by default, but can be set to a slice of `std.build.Pkg`s that your package depends on.
-//     };
-// }
